1b383fee219b1d7c499c99d0aefef3e8595c8043
大喜理風 <daikiriuta0926@gmail.com>
2021-07-05 16:47:22 +0900

    no message

--- app/src/main/java/com/riuta/recyclerswipe2/SwipeAdapter.kt
+++ app/src/main/java/com/riuta/recyclerswipe2/SwipeAdapter.kt
@@ -1,13 +1,10 @@
 package com.riuta.recyclerswipe2
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 
